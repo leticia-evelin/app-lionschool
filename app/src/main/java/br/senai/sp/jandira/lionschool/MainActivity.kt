d59e005f8df8f38c53b065a9b1f5699d33230df6
leticia-evelin <ljkeleticia@gmail.com>
2023-05-23 09:48:17 -0300

    primeira tela concluida

--- app/src/main/java/br/senai/sp/jandira/lionschool/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/MainActivity.kt
@@ -1,18 +1,28 @@
 package br.senai.sp.jandira.lionschool
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.lionschool.ui.theme.LionSchoolTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,9 +45,67 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
 
-    Column(modifier = Modifier.padding(32.dp)) {
+    val context = LocalContext.current.applicationContext
+    val context2 = LocalContext.current
+
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(Color(51, 71, 176))) {
+
+        Row(modifier = Modifier.padding(32.dp)) {
+            Text(text = stringResource(id = R.string.app_name),
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                color = Color.White
+            )
+        }
+        Card(modifier = Modifier.width(415.dp).height(2.dp) ,shape = RoundedCornerShape(topEnd = 2.dp), backgroundColor = Color(242, 180, 65)) {
+        }
+        Spacer(modifier = Modifier.height(95.dp))
+        Row() {
+            Image(painter = painterResource(id = R.drawable.logo),
+                alignment = Alignment.Center,
+                contentDescription = "logo lion",
+                modifier = Modifier
+                    .width(400.dp)
+                    .height(170.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(140.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Button(onClick = { val openNext = Intent(context2, HomeActivity::class.java)
+                             context2.startActivity(openNext)},
+                colors = ButtonDefaults.buttonColors(Color(242, 180, 65)),
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .width(220.dp)
+                    .height(42.dp)
+
+
+            ) {
+                Row() {
+                    Text(text = stringResource(id = R.string.get_started),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color(51, 71, 176),
+                        modifier = Modifier.clickable {
+
+                        }
+
+                    )
+                }
+        }
+
+
+        }
 
     }
+
+
+
 }
 
 @Preview(showBackground = true, showSystemUi = true)
